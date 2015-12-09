8dfc552e7bd4be266ec534c9d35440ed7e3250b1
lmaddalena <mdn.luca@gmail.com>
2015-12-10 00:10:26 +0100

    .

--- program.c
+++ program.c
@@ -27,8 +27,9 @@ typedef struct commander {
 } t_commander;
 
 // functions prototypes
+t_commander *makecommander(void);
+t_param *addparam(t_commander *cmd, char *name);
 t_option *addoption(t_commander *cmd, char *sname, char *lname, char *description, int mandatory, char *paramspec);
-t_commander *makecommander();
 void printoption(t_option *opt);
 void showusage(char *program_name, t_commander *cmd);
 int parseargs(t_commander *cmd, int argc, char *argv[]);
@@ -40,17 +41,19 @@ void showreport(t_commander *cmd);
 // main
 int main(int argc, char *argv[])
 {
-	printf("Commander is an command line parsing module writed by L. Maddalena\n");
+	printf("Commander is a command line parsing module writed by L. Maddalena\n");
 
 	t_commander *cmd = makecommander();
-	addoption(cmd, "h", "help", "output usage information", 0, NULL);
+	addparam(cmd, "source");
+	addparam(cmd, "destination");
 	addoption(cmd, "f", "foo", "the foo parameter", 0, "value");
 	addoption(cmd, "g", "gas", "the gas parameter", 0, NULL);
-	addoption(cmd, "b", "bar", "the bar parameter", 1, "0|1");
+	addoption(cmd, "b", "bar", "the bar parameter", 1, "1|2|3");
 	addoption(cmd, "z", "baz", "the baz parameter", 1, "name");
+	addoption(cmd, "h", "help", "output usage information", 0, NULL);
 
 	int p = parseargs(cmd, argc, argv);
-	
+
 	if(p == 0 || strcmp(getoptionbysname("h", cmd)->value, "1") == 0)
 	{
 		showusage(argv[0], cmd);
@@ -68,6 +71,14 @@ void showreport(t_commander *cmd)
 	printf("Commander report:\n");
 	printf("=================\n\n");
 
+	// print params
+	t_param *par;
+	for(par = cmd->params; par != NULL; par = par->next)
+		printf("param %s value: %s\n", par->name, par->value);
+
+	printf("\n");
+			
+	// print options
 	t_option *opt = NULL;
 	for(opt = cmd->options; opt != NULL; opt = opt->next)
 		printf("opt -%s --%s paramspec: '%s' mandatory: %i value: %s\n", opt->sname, opt->lname, opt->paramspec, opt->mandatory, opt->value );			
@@ -80,6 +91,7 @@ int parseargs(t_commander *cmd, int argc, char *argv[])
 	int i;
 
 	t_option *opt = NULL;
+	t_param *par = cmd->params;
 	
 	for(i = 1; i < argc; i++)
 	{		
@@ -89,7 +101,7 @@ int parseargs(t_commander *cmd, int argc, char *argv[])
 		// number of dash 
 		int dash = 0;
 
-		// count the dashes and set the pointer to the first char after they
+		// count the dashes and set the pointer to the first char after them
 		while(*c != '\0')
 		{
 			if(*c == '-')
@@ -100,31 +112,55 @@ int parseargs(t_commander *cmd, int argc, char *argv[])
 		}
 		
 	
-		// parse...
-		if(dash == 0 && opt != NULL && opt->paramspec != NULL)			// option value
-			opt->value = c;
-		else if(dash == 1)												// short form
-			opt = getoptionbysname(c, cmd);
-		else if(dash == 2)												// long form
-			opt = getoptionbylname(c, cmd);
+		// -----------------------------------------------------------------------------------------------------
+		// parsing command line arguments
+		// -----------------------------------------------------------------------------------------------------	
+	 
+		if(dash == 0 && opt == NULL)									// is not an option, nor an option value. Is a param!
+		{
+			if(par == NULL)			 
+				return 0;												// no more params required
+			else
+			{
+				par->value = c;											// set param value
+				par = par->next;										// move to next param
+			}
+		}
+		else if(dash == 0 && opt != NULL && opt->paramspec != NULL)		// no dashes: is an option value
+		{
+			opt->value = c;												// set option value
+			opt = NULL;													// wait for new option
+		}
+		else if(dash == 1)												// one dashe (-)
+		{
+			opt = getoptionbysname(c, cmd);								// get option by short name
+			if(opt == NULL) return 0;									// option not found
+			if(opt->paramspec == NULL) opt->value = "1";				// set flag to true
+				
+		}
+		else if(dash == 2)												// two dashes (--)
+		{
+			opt = getoptionbylname(c, cmd);								// get option by long name
+			if(opt == NULL) return 0;									// option not found
+			if(opt->paramspec == NULL) opt->value = "1";				// set flag to true
+		}
+
 		else															// too many dashes
 			return 0;
-			
-		// printf("%i*%s:%i",dash,c,strlen(c));
-							
-		if(opt == NULL)							// option not found
-			return 0;	
-		else
-			if(opt->paramspec == NULL)			// option doesn't have a value. Set 1 (true)
-				opt->value = "1";						
+								
 	}
+
+	// check parameters
+	for(par = cmd->params; par != NULL; par = par->next)
+		if(par->value == NULL)
+			return 0;			
 
 	// check mandatory options
 	for(opt = cmd->options; opt != NULL; opt = opt->next)
 		if(opt->mandatory == 1 && opt->value == NULL)
 			return 0;			
 	
-	return 1;									// all argument parsed. return ok.
+	return 1;															// all argument parsed. return ok.
 }
 
 //
@@ -155,7 +191,13 @@ t_option *getoptionbylname(char *lname, t_commander *cmd)
 void showusage(char *program_name, t_commander *cmd)
 {
 	printf("\n");
-	printf("Usage: %s [options]\n", program_name);
+	printf("Usage: %s", program_name);
+	
+	t_param *par = NULL;
+	for(par = cmd->params; par != NULL; par = par->next)
+		printf(" %s", par->name);
+	
+	printf(" [options]\n");
 	printf("\n");
 	printf("Options:\n");
 	printf("\n");
@@ -167,7 +209,7 @@ void showusage(char *program_name, t_commander *cmd)
 
 //
 // makecommander
-t_commander *makecommander()
+t_commander *makecommander(void)
 {
 	t_commander *cmd = malloc(sizeof(t_commander));
 	cmd->options = NULL;
@@ -176,7 +218,31 @@ t_commander *makecommander()
 }
 
 //
-// makeoption
+// addparam
+t_param *addparam(t_commander *cmd, char *name)
+{
+	t_param *par = malloc(sizeof(t_param));
+	
+	par->name = name;
+	par->value = NULL;
+	par->next = NULL;
+	
+	if(cmd->params == NULL)
+		cmd->params = par;
+	else
+	{
+		t_param *lastpar = NULL;
+		for(lastpar = cmd->params; lastpar->next != NULL; lastpar = lastpar->next)
+			;
+		lastpar->next = par;
+		
+	}
+	
+	return par;
+}
+
+//
+// addoption
 t_option *addoption(t_commander *cmd, char *sname, char *lname, char *description, int mandatory, char *paramspec)
 {
 	t_option *opt = malloc(sizeof(t_option));
@@ -189,7 +255,7 @@ t_option *addoption(t_commander *cmd, char *sname, char *lname, char *descriptio
 	opt->next = NULL;
 	
 	if(paramspec == NULL)
-		opt->value = "0";		// option doesen't have a value: set to false
+		opt->value = "0";		// option doesen't have a value, is a flag option: set to false
 	else
 		opt->value = NULL;		// option must have a value: set to NULL (it will be get from command line args)
 

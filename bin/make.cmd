9f711ee114914f2b584dddad3ab4ba60da0e785f
lmaddalena <mdn.luca@gmail.com>
2017-12-11 10:28:31 +0100

    Compilazione con simboli di DEBUG

--- bin/make.cmd
+++ bin/make.cmd
@@ -1,3 +1,3 @@
-cl ../test.c ../commander.c /Fe:testcommander.exe
+cl /Zi /Fd ../test.c ../commander.c /Fe:testcommander.exe
 
-cl ../fibonacci.c ../commander.c /Fe:fibo.exe
+cl /Zi /Fd ../fibonacci.c ../commander.c /Fe:fibo.exe

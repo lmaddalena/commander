e7081baa4b19642010d55a553d92649e7a10106b
lmaddalena <mdn.luca@gmail.com>
2018-01-30 00:28:03 +0100

    updated .gitignore

--- make.cmd
+++ make.cmd
@@ -1,3 +1,3 @@
-cl /Zi /Fd ../test.c ../commander.c /Fe:testcommander.exe
+cl /Zi /Fd src\test.c src\commander.c /Fe:build\testcommander.exe
 
-cl /Zi /Fd ../fibonacci.c ../commander.c /Fe:fibo.exe
+cl /Zi /Fd src\fibonacci.c src\commander.c /Fe:build\fibo.exe
